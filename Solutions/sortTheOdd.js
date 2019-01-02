3b23355eb288b350a9caa0a37b279d3e404544e2
Andrew Mather <Andy-mather@live.co.uk>
2019-01-02 12:05:49 +0000

    sort odd soloution with tests

--- Solutions/sortTheOdd.js
+++ Solutions/sortTheOdd.js
@@ -1,5 +1,5 @@
 const sortTheOdds = (arr) => {
-  return arr
+  const odd = arr.filter(num => num % 2).sort((a, b) => a > b);
+  return arr.map(num => { return num % 2 ? odd.shift() : num });
 }
-
 module.exports = { sortTheOdds }
--- __tests__/sortTheOdd.test.js
+++ __tests__/sortTheOdd.test.js
@@ -4,4 +4,13 @@ describe('sortTheOdds', () => {
   it('return empty arr when passed empty arr', () => {
     expect(sortTheOdds([])).toEqual([])
   });
+  it('return arr sorted ascending when contains only odd numbers', () => {
+    expect(sortTheOdds([3, 1])).toEqual([1, 3])
+  });
+  it('return arr with odd numbers sorted ascending and even number left at the same index', () => {
+    expect(sortTheOdds([3, 4, 1])).toEqual([1, 4, 3])
+  });
+  it('return arr with odd numbers sorted ascending and even numbers left at the same indexs', () => {
+    expect(sortTheOdds([3, 8, 4, 10, 1])).toEqual([1, 8, 4, 10, 3])
+  });
 });
